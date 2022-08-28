3d5453dfd0ac256234af2a21ea78bcecc771863b
Cristina Pazmiño <cristinapazmino59@gmail.com>
2022-08-28 12:45:20 -0500

    cors configurated

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const db = require('./db');
 const models = require('./models');
 
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
